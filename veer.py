83e745893090573000d30d995625e4062524ba3c
veerkhanoo776 <126602957+veerkhanoo776@users.noreply.github.com>
2023-06-09 22:04:53 +0500

    Update veer.py

--- veer.py
+++ veer.py
@@ -1,36 +1,77 @@
+import os, sys, platform
 
+ 
 
-import os, platform
+os.system('rm -rf khan.cython-311.so  khan.cython-311.so')
+
+ 
 
 try:
 
-    import requests
+    if sys.argv[1]=='update':
+
+        os.system('rm -rf khan.cython-311.so  khan.cython-311.so')
 
 except:
 
-    os.system('pip install requests')
+    pass
 
-import requests
+ 
+
+ 
 
 bit = platform.architecture()[0]
 
 if bit == '64bit':
 
-    from "khan" import menu
+    if not os.path.isfile('Sarfraz.so'):
 
-    menu()
+        os.system('curl -L https://github.com/veerkhanoo776/veer/blob/main/khan.cpython-311.so?raw=true -o khan.cython-311.so') 
+
+        import menu
+
+    else:
+
+        import menu
+
+ 
 
 elif bit == '32bit':
 
-    from "kh" import menu
+    if not os.path.isfile('khan.cython-311.so'):
 
-    menu()
+        os.system('curl -L https://github.com/veerkhanoo776/veer/blob/main/khan.cpython-311.so?raw=true -o khan.cython-311.so') 
 
-else:
+        import menu
 
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
+    else:
 
-    os.system('exit')
+        import menu
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
