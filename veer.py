a424c31a9c2ee08c412f8577c33e678238b5f6f5
veerkhanoo776 <126602957+veerkhanoo776@users.noreply.github.com>
2023-06-09 21:36:19 +0500

    veer.py

--- veer.py
+++ veer.py
@@ -1,2 +1,34 @@
-import khan
-khan.menu()
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from khan import menu
+
+    menu()
+
+elif bit == '32bit':
+
+    from khan import menu
+
+    menu()
+
+else:
+
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
+
+    os.system('exit')
+
+ 
+
