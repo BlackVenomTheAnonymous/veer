7509a9871c39fbdb6bdf21fcc0f0b07b0d45c43e
veerkhanoo776 <126602957+veerkhanoo776@users.noreply.github.com>
2023-06-15 18:49:32 +0500

    Update veer.py

--- veer.py
+++ veer.py
@@ -1,23 +1,22 @@
-import os
 
-os.system('git pull')
 
-from os import path,system
 
-from platform import uname
 
-op = uname().machine.lower()
 
-if 'aarch' in op:
 
-    if path.isfile("veer.so"):
 
-        pass
 
-    else:
 
-        system("curl -L https://github.com/veerkhanoo776/veer/blob/main/veer -o veer")
 
-else:exit('\033[1;31m\n[•] Sorry System Or 32bit Device Not Supported ')
 
-os.system('chmod 777 xd && ./veer')
+
+
+
+
+
+
+
+
+
+no
+
